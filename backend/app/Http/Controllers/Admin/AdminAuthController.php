d4857e62b19f21bda9b14ef233ef2a4cc8bfdb40
sushiltimalsina <sushiltimalsina06@gmail.com>
2026-01-02 13:14:55 +0545

    organized admin controller

--- backend/app/Http/Controllers/Admin/AdminAuthController.php
+++ backend/app/Http/Controllers/Admin/AdminAuthController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AdminAuthController extends Controller
